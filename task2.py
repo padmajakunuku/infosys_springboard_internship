9ef45c298aab6fcea37ebdeab0d26dfadbf78f18
padmajakunuku <padmajak004@gmail.com>
2025-10-07 11:21:05 +0530

    this task 2 has code to find avg marks per subject of given data

--- task2.py
+++ task2.py
@@ -1,6 +1,6 @@
 #read a student.csv file and find avg marks per subject
 import pandas as pd
-data=pd.read_csv(student.csv)
+data=pd.read_csv("student.csv")
 avg_per_subject = data.drop("Name", axis=1).mean()
 print("Average marks per subject: ")
 print(avg_per_subject)
